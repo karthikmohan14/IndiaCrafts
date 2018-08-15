d45cb8bf9263a6f1a2eb2e65d2af5ea5321d8604
karthikmohan14 <karthikmkochi@gmail.com>
2018-08-15 12:24:01 +0530

    defining userSchema in mongoose

---
+++ models/user.js
@@ -0,0 +1,63 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+// user schema attributes
+var UserSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    password: String,
+    profile: {
+        name: {
+            type: String,
+            default: ''
+        },
+        picture: {
+            type: String,
+            default: ''
+        },
+        address: String,
+        history: [{
+            date: Date,
+            paid: {
+                type: Number,
+                default: 0
+            },
+            // item: {
+            //     type:Schema.Types.ObjectId,
+            //     ref:''
+            // }
+        }]
+    }
+});
+
+// password hash before saving to database
+UserSchema.pre('save', function (next) {
+
+    var user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// compare password for login
+
+UserSchema.methods.comparePassword = function (password) {
+
+    return bcrypt.compareSync(password, this.password);
+};
