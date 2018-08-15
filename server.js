ff65bee30fd60156f60ceaa55873df5d02163c7a
karthikmohan14 <karthikmkochi@gmail.com>
2018-08-15 14:48:21 +0530

    verifying create-user with postman

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-
+var User = require('./models/user');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.listen(3000, function (err) {
     if (err) {
@@ -21,12 +26,26 @@ mongoose.connect('mongodb://root:!nd1aCraft5@ds121312.mlab.com:21312/indiacrafts
     }
 });
 
-app.get('/', function (req, res) {
-    var message = "welcome";
-    res.json(message);
-});
+// app.get('/', function (req, res) {
+//     var message = "welcome";
+//     res.json(message);
+// });
 
 
-app.get('/about', function (req, res) {
-    res.json("welcome to about page");
+// app.get('/about', function (req, res) {
+//     res.json("welcome to about page");
+// });
+
+app.post('/create-user', function (req, res, next) {
+
+    var user = new User();
+    user.profile.name = req.body.name;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.json('succefully created new user');
+    });
 });
